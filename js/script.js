462fdab9ef94b4f3d92f6a5d0a351dcd91e75e1a
Dan <daniel.munozdev@gmail.com>
2017-10-12 16:02:36 -0700

    Add click event listener to remove the shouted text

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@ $('form#form').submit(function(event) {
   event.preventDefault();
   const shoutInput = $('input#shout').val().toUpperCase();
   $('.container').prepend('<h1>' + shoutInput + '</h1>');
+  $('h1').first().click(function() {
+    $(this).fadeOut(300, function() {
+      $(this).remove();
+    });
+  });
 });
